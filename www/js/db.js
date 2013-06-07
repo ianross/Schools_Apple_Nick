4599ef5c43bb931ebac00517d07cd6de203d8d61
ian <iancalligeros@gmail.com>
2013-06-07 20:22:59 +1000

    speightpls

--- www/js/db.js
+++ www/js/db.js
@@ -128,7 +128,7 @@ function updateEmailSuccess(){
 function addTeacherSuccess(){
 
     navigator.notification.alert(
-        'Email Address Successfully Updated',  // message
+        'Teacher Email Address Successfully Updated',  // message
         function() {},         // callback
         'Email Updated',            // title
         'Close'                  // buttonName
